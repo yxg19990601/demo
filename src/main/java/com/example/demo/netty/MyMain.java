e2b189d3fdd920a10a994a687f411e1745ab0e58
yxgeclipse <175@qq.com>
2018-08-13 17:28:37 +0800

    add myclass

---
+++ src/main/java/com/example/demo/netty/MyMain.java
@@ -0,0 +1,11 @@
+package com.example.demo.netty;
+
+/**
+ * @Author YXG
+ * @Date 2018-08-13 17:27
+ */
+public class MyMain {
+    public static void main(String[] args) {
+        System.out.println("MyMain.main");
+    }
+}
